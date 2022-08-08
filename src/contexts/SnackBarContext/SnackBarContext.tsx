548e99a85042c5e2f966e274978ccf6318930711
vojtech.holan <vojtech.holan@3pillarglobal.com>
2022-08-08 08:56:04 +0200

    refactoring

--- src/contexts/SnackBarContext/SnackBarContext.tsx
+++ src/contexts/SnackBarContext/SnackBarContext.tsx
@@ -1,10 +1,4 @@
-import {
-  createContext,
-  ReactElement,
-  useEffect,
-  useMemo,
-  useState,
-} from 'react'
+import { createContext, ReactElement, useMemo, useState } from 'react'
 
 type SnackBarContextProps = {
   children: ReactElement | ReactElement[]
